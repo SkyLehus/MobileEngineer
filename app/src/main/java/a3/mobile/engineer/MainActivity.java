ce1f85d6c81ebad0282a1419d59397a23780e767
Babkin <babkin_a@mail.ru>
2015-11-05 22:20:44 +0300

    список с тестовыми данными

--- app/src/main/java/a3/mobile/engineer/MainActivity.java
+++ app/src/main/java/a3/mobile/engineer/MainActivity.java
@@ -17,14 +17,74 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    static final private int RESULT_CODE = 0;
+    private static final String INC_NUMBER = "catname"; // Верхний текст
+    private static final String DESCRIPTION = "description"; // ниже главного
+    private ArrayList<HashMap<String, Object>> mList;
+
     private void refreshData(){
         SSMSErvice ssm = new SSMSErvice(MainActivity.this, "Administrator","Qwerty123");
         ssm.checkConnection();
+
+
     }
+
+    private void refreshDataHandler() {
+        ListView listView = (ListView) findViewById(R.id.mainListView);
+
+        // создаем массив списков
+        mList = new ArrayList<HashMap<String, Object>>();
+        HashMap<String, Object> hm;
+
+        hm = new HashMap<>();
+        hm.put(INC_NUMBER, "INC000000111"); // Название
+        hm.put(DESCRIPTION, "Рыжий и хитрый"); // Описание
+        mList.add(hm);
+
+        hm = new HashMap<>();
+        hm.put(INC_NUMBER, "INC000000111");
+        hm.put(DESCRIPTION, "Слушает да ест");
+        mList.add(hm);
+
+        hm = new HashMap<>();
+        hm.put(INC_NUMBER, "INC000000521");
+        hm.put(DESCRIPTION, "Спит и мурлыкает");
+        mList.add(hm);
+
+        hm = new HashMap<>();
+        hm.put(INC_NUMBER, "INC000000478");
+        hm.put(DESCRIPTION, "Болеет за Барселону");
+        mList.add(hm);
+
+        SimpleAdapter adapter = new SimpleAdapter(this, mList,
+                R.layout.main_list_item, new String[]{INC_NUMBER, DESCRIPTION},
+                new int[]{R.id.itemNumber, R.id.itemDescription});
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(itemClickListener);
+    }
+
+    AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            HashMap<String, Object> itemHashMap = (HashMap <String, Object>) parent.getItemAtPosition(position);
+            String titleItem = itemHashMap.get(INC_NUMBER).toString();
+            String descriptionItem = itemHashMap.get(DESCRIPTION).toString();
+            Toast.makeText(getApplicationContext(),
+                    "Вы выбрали " + titleItem + ". Он " + descriptionItem, Toast.LENGTH_SHORT).show();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,11 +135,20 @@ public class MainActivity extends AppCompatActivity
 
         if (versionDB != versionName) {
             // переход к регистрации
+
             Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
-            startActivity(intent);
+            startActivityForResult(intent,RESULT_CODE);
         } else {
             refreshData();
         }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+       refreshDataHandler();
     }
 
     @Override
