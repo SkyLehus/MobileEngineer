a7a9c3333a5b1240f29807a8d37e067bc9d907d8
Babkin <babkin_a@mail.ru>
2015-11-05 21:04:22 +0300

    интерфейс регистрации (без данных)

--- app/src/main/java/a3/mobile/engineer/RegistrationActivity.java
+++ app/src/main/java/a3/mobile/engineer/RegistrationActivity.java
@@ -6,6 +6,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -17,6 +19,26 @@ public class RegistrationActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
 
+    }
+
+    public void onRegister(View view){
+
+        AutoCompleteTextView eLogin = (AutoCompleteTextView)findViewById(R.id.editLogin);
+        AutoCompleteTextView ePassw = (AutoCompleteTextView)findViewById(R.id.editPassword);
+        AutoCompleteTextView ePin = (AutoCompleteTextView)findViewById(R.id.editPin);
+
+
+
+
+        clearFields();
+        finish();
+    }
+
+    private void clearFields() {
+        AutoCompleteTextView eLogin = (AutoCompleteTextView)findViewById(R.id.editLogin) ;
+        AutoCompleteTextView ePassw = (AutoCompleteTextView)findViewById(R.id.editPassword);
+        AutoCompleteTextView ePin = (AutoCompleteTextView)findViewById(R.id.editPin);
+        eLogin.setText(""); ePassw.setText(""); ePin.setText("");
     }
 
 }
